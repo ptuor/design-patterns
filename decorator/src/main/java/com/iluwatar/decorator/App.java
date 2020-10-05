4efcb5c26373d0dc3e0832eb523c5f3906a931d3
sandromuggli <sandro.muggli@gmail.com>
2020-10-05 14:38:50 +0200

    solution for ex_2_4_decorator

--- decorator/src/main/java/com/iluwatar/decorator/App.java
+++ decorator/src/main/java/com/iluwatar/decorator/App.java
@@ -62,15 +62,16 @@ public class App {
     clubbed.fleeBattle();
     LOGGER.info("Clubbed troll power {}.\n", clubbed.getAttackPower());
 
-
     LOGGER.info("A troll with two huge clubs surprises you.");
-    // 1.) @todo: instanziiere einen 'doubleClubbed' troll der 2 Keulen schwingt.
+    Troll doubleClubbed = new ClubbedTroll(new ClubbedTroll(new SimpleTroll()));
+    doubleClubbed.attack();
+    doubleClubbed.fleeBattle();
+    LOGGER.info("Double clubbed troll power {}.\n", doubleClubbed.getAttackPower());
 
     LOGGER.info("A troll with a huge club and an arch surprises you.");
-    /*
-       2.) @todo: instanziiere einen 'clubbedArcherTroll' der eine Keule schwingt und einen Bogen hat.
-        Hinweis: Erstelle dazu einen neuen ConcreteDecorator 'ArchTroll'
-     */
-
+    Troll clubbedArcherTroll = new ArchTroll(new ClubbedTroll(new SimpleTroll()));
+    clubbedArcherTroll.attack();
+    clubbedArcherTroll.fleeBattle();
+    LOGGER.info("Clubbed arch troll power {}.\n", clubbedArcherTroll.getAttackPower());
   }
 }
