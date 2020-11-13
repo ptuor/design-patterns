1dc62fc73b0023ef49609c8dc5be2c0c54b6f540
samu <sandro.muggli@finnova.com>
2020-11-13 17:55:19 +0100

    starting point ex_4_1_proxy

--- proxy/src/first/App.java
+++ proxy/src/first/App.java
@@ -1,17 +1,23 @@
 package first;
 
-import second.Picture;
 import second.ProxyPicture;
 
 public class App {
     public static void main(String[] args) {
-        Picture image = new ProxyPicture("largePicture.jpg");
+
+        /**
+         * Instanziierung von RealPicture ist nicht möglich, da die Klasse protected ist und wir diese nicht editieren oder verschieben dürfen.
+         * Auch die Klasse App muss im aktuellen Package bleiben.
+         * Um RealPicture zu n utzen hat ein Kollege einen Proxy gebaut (ProxyPicture), leider hat er das Pattern nicht so ganz verstanden und dadurch den Proxy nicht optimal gestaltet.
+         * Verbessere den Proxy
+         */
+        ProxyPicture image = new ProxyPicture("largePicture.jpg");
 
         //picture will be loaded from server
-        image.display();
+        image.draw();
         System.out.println("now read from cache");
 
-        //cached picture
-        image.display();
+        //cached picture (do not download picture again)
+        image.draw();
     }
 }
--- proxy/src/second/ProxyPicture.java
+++ proxy/src/second/ProxyPicture.java
@@ -1,18 +1,13 @@
 package second;
 
-public class ProxyPicture implements Picture {
-    private RealPicture realImage;
+public class ProxyPicture {
     private String fileName;
 
-    public ProxyPicture(String fileName){
+    public ProxyPicture(String fileName) {
         this.fileName = fileName;
     }
 
-    @Override
-    public void display() {
-        if(realImage == null){
-            realImage = new RealPicture(fileName);
-        }
-        realImage.display();
+    public void draw() {
+        new RealPicture(fileName).display();
     }
 }
